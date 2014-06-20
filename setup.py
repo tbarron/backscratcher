a7a5ce024ef622a3e48d0ff802e33c762d04fe6a
Tom Barron <tom.barron@comcast.net>
2014-06-19 21:40:09 -0400

    Moving toward using distutils/setup.py, etc.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='backscratcher',
+      version='learn',
+      packages=['bscr'],
+      scripts=['bin/fx'],
+      author="Tom Barron",
+      author_email='tom.barron@comcast.net',
+      url='https://github.com/tbarron/backscratcher/'
+      )
