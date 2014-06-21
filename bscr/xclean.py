6a1d44ad6adf3b4b2502c6d72e920af9fac44d55
Tom Barron <tom.barron@comcast.net>
2014-06-21 10:00:13 -0400

    Writing tests for xclean.

--- bscr/xclean.py
+++ bscr/xclean.py
@@ -1,5 +1,6 @@
 import optparse
 import os
+import pdb
 import re
 import sys
 
@@ -8,7 +9,7 @@ def main(args=None):
 
     if args is None:
         args = sys.argv
-        
+
     p = optparse.OptionParser()
     p.add_option('-d', '--debug',
                  action='store_true', default=False, dest='debug',
@@ -16,20 +17,23 @@ def main(args=None):
     p.add_option('-n', '--dry-run',
                  action='store_true', default=False, dest='dryrun',
                  help='just report')
+    p.add_option('-p', '--pattern',
+                 action='store', default=None, dest='pattern',
+                 help='file matching regexp')
     p.add_option('-r', '--recursive',
                  action='store_true', default=False, dest='recursive',
                  help='whether to descend directories')
     (o, a) = p.parse_args(args)
 
-    if 0 == len(a):
-        cleanup('.', o.dryrun)
+    if 0 == len(a[1:]):
+        cleanup('.', o.dryrun, o.pattern, o.recursive)
     else:
-        for dir in a:
-            cleanup(dir, o.dryrun)
+        for dir in a[1:]:
+            cleanup(dir, o.dryrun, o.pattern, o.recursive)
 
 # -----------------------------------------------------------------------------
-def cleanup(dir, dryrun=False):
-    flist = find_files(dir, o.recursive)
+def cleanup(dir, dryrun=False, pattern=None, recursive=False):
+    flist = find_files(dir, pattern, recursive)
     if dryrun:
         print("Without --dryrun, would remove:")
         for fname in flist:
@@ -40,14 +44,16 @@ def cleanup(dir, dryrun=False):
             os.unlink(fname)
 
 # -----------------------------------------------------------------------------
-def find_files(dir, recursive=False):
+def find_files(dir, pattern=None, recursive=False):
+    if pattern is None:
+        pattern = '.*~'
     rval = []
     if recursive:
         for root, dlist, flist in os.walk(dir):
-            fl = [os.path.join(root, f) for f in flist if re.match('.*~', f)]
+            fl = [os.path.join(root, f) for f in flist if re.match(pattern, f)]
             rval.extend(fl)
     else:
         rval = [os.path.join(dir, f) for f in os.listdir(dir)
-                if re.match('.*~', f)]
+                if re.match(pattern, f)]
 
     return rval
--- test/test_xclean.py
+++ test/test_xclean.py
@@ -1,47 +1,192 @@
 import bscr.xclean
 import os
 import shutil
+import StringIO as sio
+import sys
 import unittest
 
+# -----------------------------------------------------------------------------
 def pj(*args):
+    """
+    pathjoin -- convenience wrapper for os.path.join()
+    """
     return os.path.join(*args)
 
-def touch(filepath):
+# -----------------------------------------------------------------------------
+def touch(filepath, times=None):
+    """
+    touch -- ensure file exists and update its *times* (atime, mtime)
+    """
     open(filepath, 'a').close()
-    os.utime(filepath, None)
+    os.utime(filepath, times)
 
+class StdoutExcursion(object):
+    def __init__(self):
+        self.stdout = sys.stdout
+
+    def __enter__(self):
+        sys.stdout = sio.StringIO()
+        return sys.stdout.getvalue
+
+    def __exit__(self, type, value, traceback):
+        sys.stdout.close()
+        sys.stdout = self.stdout
+
+# -----------------------------------------------------------------------------
 class TestClean(unittest.TestCase):
     testdir = "/tmp/test_xclean"
-    testfiles = [pj(testdir, 'xxx~'),
-                 pj(testdir, '.yyy~'),
-                 pj(testdir, 'sub', 'basement~'),
-                 pj(testdir, 'sub', '.floor~')]
+    tilde = [pj(testdir, 'xxx~'),
+             pj(testdir, '.yyy~')]
+    stilde = [pj(testdir, 'sub', 'basement~'),
+              pj(testdir, 'sub', '.floor~')]
+    ntilde = [pj(testdir, 'nomatch.txt')]
+    nstilde = [pj(testdir, 'sub', 'nosuchfile')]
+    testfiles = tilde + stilde + ntilde + nstilde
+
+    # -------------------------------------------------------------------------
     def setUp(self):
         os.makedirs(pj(self.testdir, 'sub'))
         for fp in self.testfiles:
             touch(fp)
 
+    # -------------------------------------------------------------------------
     def tearDown(self):
         shutil.rmtree(self.testdir)
         
+    # -------------------------------------------------------------------------
+    def test_main_dr_np_nr(self):
+        with StdoutExcursion() as sio:
+            bscr.xclean.main(['bin/xclean', '-n', self.testdir])
+            result = sio()
+        self.assertIn('Without --dryrun, would remove', result)
+        for fn in self.tilde:
+            self.assertIn(fn, result,
+                          "Expected '%s' in '%s'" % (fn, result))
+        for fn in self.ntilde:
+            self.assertNotIn(fn, result,
+                             "Expected '%s' to not be in '%s'" % (fn, result))
+        for fn in self.stilde:
+            self.assertNotIn(fn, result,
+                             "Expected '%s' to not be in '%s'" % (fn, result))
+        for fn in self.nstilde:
+            self.assertNotIn(fn, result,
+                             "Expected '%s' to not be in '%s'" % (fn, result))
+
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_dr_p_nr(self):
+        self.fail('construction')
+    
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_dryrun_nopat_r(self):
+        self.fail('construction')
+
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_dryrun_pat_r(self):
+        self.fail('construction')
+        
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_nodr_nopat(self):
+        self.fail('construction')
+
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_nodr_pat(self):
+        self.fail('construction')
+        
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_nodr_nopat_r(self):
+        self.fail('construction')
+
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_main_nodr_pat_r(self):
+        pass
+
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_cleanup_dr_np(self):
+        pass
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_cleanup_dr_p(self):
+        pass
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_cleanup_ndr_np(self):
+        pass
+    # -------------------------------------------------------------------------
+    @unittest.skip('construction')
+    def test_cleanup_ndr_p(self):
+        pass
+    # -------------------------------------------------------------------------
     def test_find_files(self):
         fl = bscr.xclean.find_files(self.testdir)
-        self.assertIn(self.testfiles[0], fl,
-                      "Expected %s in %s" % (self.testfiles[0], fl))
-        self.assertIn(self.testfiles[1], fl,
-                      "Expected %s in %s" % (self.testfiles[0], fl))
+        for fn in self.tilde:
+            self.assertIn(fn, fl,
+                          "Expected %s in %s" % (fn, fl))
+        for fn in self.stilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.ntilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.nstilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        
+    # -------------------------------------------------------------------------
+    def test_find_files_r(self):
+        fl = bscr.xclean.find_files(self.testdir, recursive=True)
+        for fn in self.tilde:
+            self.assertIn(fn, fl,
+                          "Expected %s in %s" % (fn, fl))
+        for fn in self.stilde:
+            self.assertIn(fn, fl,
+                          "Expected %s in %s" % (fn, fl))
+        for fn in self.ntilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.nstilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        
+    # -------------------------------------------------------------------------
+    def test_find_files_p(self):
+        fl = bscr.xclean.find_files(self.testdir, pattern='no.*')
+        for fn in self.tilde:
+            self.assertNotIn(fn, fl,
+                          "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.stilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.ntilde:
+            self.assertIn(fn, fl,
+                          "Expected %s in %s" % (fn, fl))
+        for fn in self.nstilde:
+            self.assertNotIn(fn, fl,
+                             "Expected %s to not be in %s" % (fn, fl))
+        
+    # -------------------------------------------------------------------------
+    def test_find_files_p_r(self):
+        fl = bscr.xclean.find_files(self.testdir, pattern='no.*',
+                                    recursive=True)
+        for fn in self.tilde:
+            self.assertNotIn(fn, fl,
+                          "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.stilde:
+            self.assertNotIn(fn, fl,
+                          "Expected %s to not be in %s" % (fn, fl))
+        for fn in self.ntilde:
+            self.assertIn(fn, fl,
+                             "Expected %s in %s" % (fn, fl))
+        for fn in self.nstilde:
+            self.assertIn(fn, fl,
+                             "Expected %s in %s" % (fn, fl))
 
-        self.assertNotIn(self.testfiles[2], fl,
-                         "Expected %s not in %s" % (self.testfiles[0], fl))
-        self.assertNotIn(self.testfiles[3], fl,
-                         "Expected %s not in %s" % (self.testfiles[0], fl))
         
-    def test_recursive(self):
-        self.fail('construction')
-        
-    def test_xclean_pattern_def(self):
-        self.fail('construction')
-        
-    def test_xclean_pattern_env(self):
-        self.fail('construction')
         
