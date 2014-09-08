05253e90f8294af4f481952acc8cf6e4b272ecba
Tom Barron <tom.barron@comcast.net>
2014-09-07 21:47:19 -0400

    Now we're trying to find bscr root based on the location of setuptools

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import site
 import sys
 import time
 
-# bscr_root = os.path.join(distutils.old_get_python_lib(), "bscr")
-# bscr_root = os.path.join(os.path.dirname(site.__file__), "bscr")
+bscr_root = os.path.join(
+    os.path.dirname(os.path.dirname(setuptools.__file__)), "bscr")
 distutils.file_util.copy_file("README.md", "README")
 scripts = ["align",
            "ascii",
@@ -40,7 +40,7 @@ setup(name='backscratcher',
       author="Tom Barron",
       author_email='tom.barron@comcast.net',
       url='https://github.com/tbarron/backscratcher/',
-      data_files=[('', ["README"])],
+      data_files=[(bscr_root, ["README"])],
       install_requires=['pexpect == 3.3'],
       tests_require=['pep8==1.5.7',
                      'python-termstyle==0.1.10',
