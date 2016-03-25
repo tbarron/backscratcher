104e26023e22e5b278e8bb4f5930cf140b583066
Tom Barron <tom.barron@comcast.net>
2016-03-25 18:55:49 -0400

    Replaced ad hoc code checking with standard static analyzer pylint

---
+++ test/test_zlint.py
@@ -0,0 +1,27 @@
+"""
+This file checks for best practice compliance.
+ - Does my code conform to PEP8?
+ - Do I have any inadvertent duplicate routines?
+ - Do I have routines with no __doc__?
+
+Conveniently, pylint takes care of all of this
+"""
+import glob
+import inspect
+import os
+import pdb
+import pexpect
+import re
+import sys
+from bscr import testhelp as th
+import types
+import unittest
+from bscr import util as U
+
+# -----------------------------------------------------------------------------
+def test_pylint():
+    """
+    Run pylint to check the quality of the code
+    """
+    result = pexpect.run("pylint bscr")
+    assert result == ''
