62a10425445922eb3d5725e967f072a382d016c2
Tom Barron <tom.barron@comcast.net>
2016-04-13 07:01:18 -0400

    Pylint tweaks for mcal.py

--- bscr/mcal.py
+++ bscr/mcal.py
@@ -9,12 +9,17 @@ Options:
     -m <month>  Month of calendar
     -w          Print a wide calendar showing three months
 """
-import docopt
 import pdb
 import sys
 import time
 
+import docopt
+
+# -----------------------------------------------------------------------------
 def main(args=None):
+    """
+    This is the entry point
+    """
     args = args or sys.argv
     opts = docopt.docopt(__doc__, args[1:])
     if opts['-d']:
@@ -34,7 +39,11 @@ def main(args=None):
     for line in mlist:
         print line
 
+# -----------------------------------------------------------------------------
 def gen_month(year, month):
+    """
+    Generate the layout for a month
+    """
     date = first_day(year, month)
     rval = []
     rval.append(title(year, month))
@@ -42,19 +51,19 @@ def gen_month(year, month):
 
     line = ''
     # blanks before first day
-    for day in range(date.tm_wday):
+    for _ in range(date.tm_wday):
         line += '   '
 
     # build the rest of the first line
     base = date.tm_wday
-    for day in range(base, 7):
+    for _ in range(base, 7):
         line += ' {0:2}'.format(date.tm_mday)
         date = next_day(date)
     rval.append(line)
 
     while date.tm_mon == month:
         line = ''
-        for day in range(7):
+        for _ in range(7):
             if date.tm_mon != month:
                 line += '   '
             else:
@@ -66,15 +75,28 @@ def gen_month(year, month):
         rval.append(' ' * 21)
     return rval
 
+# -----------------------------------------------------------------------------
 def title(year, month):
+    """
+    Generate a title for a month display containing the *year* and *month*
+    """
     rval = "{0}.{1:02}".format(year, month)
     return rval.center(21)
 
+# -----------------------------------------------------------------------------
 def week_header():
+    """
+    Generate the header line for a month display
+    """
     wdnames = " Mo Tu We Th Fr Sa Su"
     return wdnames
 
+# -----------------------------------------------------------------------------
 def first_day(year=None, month=None):
+    """
+    Given *year* and *month*, return the epoch time at the begining of the
+    first day of the month
+    """
     today = time.localtime()
     if year is None:
         year = today.tm_year
@@ -85,19 +107,28 @@ def first_day(year=None, month=None):
     rval = time.localtime(epoch)
     return rval
 
+# -----------------------------------------------------------------------------
 def next_day(date):
+    """
+    Given a specific *date*, return the successor date
+    """
     epoch = time.mktime((date.tm_year, date.tm_mon, date.tm_mday+1,
                          0, 0, 0, 0, 0, 0))
     rval = time.localtime(epoch)
     return rval
 
+# -----------------------------------------------------------------------------
 def next_month(year, month):
+    """
+    Given *year* and *month*, return the successor year and month
+    """
     epoch = time.mktime((year, month+1, 1, 0, 0, 0, 0, 0, 0))
-    next = time.localtime(epoch)
-    year = next.tm_year
-    month = next.tm_mon
+    succ = time.localtime(epoch)
+    year = succ.tm_year
+    month = succ.tm_mon
     return year, month
 
+# -----------------------------------------------------------------------------
 if __name__ == '__main__':
     main(sys.argv)
 
