815ddd548bb97c60de4c23482790795de0740e9c
Tom Barron <tom.barron@comcast.net>
2016-08-28 21:57:10 -0400

    - Add tests for pstrack

--- test/test_pstrack.py
+++ test/test_pstrack.py
@@ -1,10 +1,33 @@
 """
 Tests for pstrack
 """
+import os
 import pytest
 
-def test_pstrackNeedsTests():
+import pexpect
+
+from bscr import pstrack
+
+
+# -----------------------------------------------------------------------------
+def test_get_process_list():
     """
-    Need tests for pstrack
+    The dict returned by get_process_list should contain the current process
+    and its parent process
     """
-    pytest.fail('pstrack needs tests')
+    pytest.debug_func()
+    procl = pstrack.get_process_list()
+    spid = str(os.getpid())
+    assert spid in procl
+    assert spid == procl[spid]['pid']
+    assert str(os.getppid()) == procl[spid]['ppid']
+
+
+# -----------------------------------------------------------------------------
+def test_execution():
+    """
+    Does pstrack run successfully?
+    """
+    pytest.skip()
+    result = pexpect.run('pstrack')
+    assert 'Traceback' not in result
