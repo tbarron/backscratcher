bcc82f0e3cf5ad50ddd746c7f0e7f51a500ddf99
Tom Barron <tusculum@gmail.com>
2017-05-19 12:52:55 -0400

    TEST: Make calc tests stand-alone

--- test/test_calc.py
+++ test/test_calc.py
@@ -5,64 +5,40 @@ from bscr import testhelp as th
 from bscr import util as U
 
 
-# -----------------------------------------------------------------------------
-def test_standalone():
+# -------------------------------------------------------------------------
+def test_example():
     """
-    Make these tests stand-alone
+    Example calc session
     """
-    pytest.fail("Make calc tests stand-alone")
+    cmd = pexpect.which("calc")
+    S = pexpect.spawn(cmd)
+    S.expect("> ")
+
+    S.sendline("7 + 12")
+    S.expect("> ")
+    assert "19" in S.before
+
+    S.sendline("7.988 + 28.576")
+    S.expect("> ")
+    assert "36.564000" in S.before
+
+    S.sendline("\"zap\" * 2")
+    S.expect("> ")
+    assert "zapzap" in S.before
+
+    S.sendline("exit()")
+    S.expect(pexpect.EOF)
+    S.close()
 
 
-# -----------------------------------------------------------------------------
-class TestCalc(th.HelpedTestCase):
-    # -------------------------------------------------------------------------
-    def test_example(self):
-        """
-        Example calc session
-        """
-        scrpath = U.script_location("calc")
-        S = pexpect.spawn(scrpath)
-        # S.logfile = sys.stdout
-        S.expect("> ")
-
-        S.sendline("7 + 12")
-        S.expect("> ")
-
-        assert("19" in S.before)
-
-        S.sendline("7.988 + 28.576")
-        S.expect("> ")
-
-        assert("36.564000" in S.before)
-
-        S.sendline("\"zap\" * 2")
-        S.expect("> ")
-
-        assert("zapzap" in S.before)
-
-        S.sendline("exit()")
-        S.expect(pexpect.EOF)
-        S.close()
-
-    # -------------------------------------------------------------------------
-    def test_calc_help(self):
-        """
-        Verify that 'calc --help' does the right thing
-        """
-        cmd = U.script_location("calc")
-        result = pexpect.run("%s --help" % cmd)
-        nexp = "Traceback"
-        self.assertFalse(nexp in result,
-                         "Not expecting '%s' in %s" %
-                         (nexp, U.lquote(result)))
-        exp = "Usage: calc [options]"
-        self.assertTrue(exp in result,
-                        "Expected '%s' in %s" %
-                        (exp, U.lquote(result)))
-
-    # -------------------------------------------------------------------------
-    def test_which_module(self):
-        """
-        Verify that we're importing the right align module
-        """
-        self.assertModule('bscr.calc', __file__)
+# -------------------------------------------------------------------------
+def test_calc_help():
+    """
+    Verify that 'calc --help' does the right thing
+    """
+    cmd = pexpect.which("calc")
+    result = pexpect.run("%s --help" % cmd)
+    nexp = "Traceback"
+    assert nexp not in result
+    exp = "Usage: calc [options]"
+    assert exp in result
