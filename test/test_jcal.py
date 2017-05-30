8d88b1b265aa4e0ed3e18a33b8277f75df9d3f2f
Tom Barron <tusculum@gmail.com>
2017-05-30 15:27:55 -0400

    TEST: Remove the class-based tests for jcal

--- test/test_jcal.py
+++ test/test_jcal.py
@@ -9,43 +9,10 @@ import pytest
 
 
 # -----------------------------------------------------------------------------
-def test_standalone():
 def test_jcal_help():
     """
-    Make these tests stand-alone
     Verify that 'jcal --help' does the right thing
     """
-    pytest.fail("Make {} tests stand-alone".format(__file__))
-
-
-# -----------------------------------------------------------------------------
-class JcalTest(th.HelpedTestCase):
-    # -------------------------------------------------------------------------
-    def test_EMPTY(self):
-        """
-        Nothing to test yet
-        """
-        pass
-
-    # -------------------------------------------------------------------------
-    # @unittest.skip("under construction")
-    def test_jcal_help(self):
-        """
-        Verify that 'jcal --help' does the right thing
-        """
-        cmd = U.script_location("jcal")
-        result = pexpect.run("%s help" % cmd)
-        exp = "help - show this list"
-        self.assertTrue(exp in result, "Expected '%s' in %s" %
-                        (exp, U.lquote(result)))
-
-    # -------------------------------------------------------------------------
-    # @unittest.skip("under construction")
-    def test_which_module(self):
-        """
-        Verify that we're importing the right align module
-        """
-        self.assertModule('bscr.jcal', __file__)
     result = pexpect.run("jcal help")
     exp = "help - show this list"
     assert exp in result
