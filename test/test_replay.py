3d62bed9e242ea647078017360b29d464df10437
Tom Barron <tusculum@gmail.com>
2017-08-15 06:52:37 -0400

    TEST: Stubs for replay tests

--- test/test_replay.py
+++ test/test_replay.py
@@ -1,9 +1,43 @@
 """
 Tests for replay
 """
+# import pexpect
 import pytest
 
 
+# -----------------------------------------------------------------------------
+def test_interval():
+    """
+    'replay -i 7 CMD' should run CMD and show its output every 7 seconds
+    """
+    pytest.fail('construction')
+
+
+# -----------------------------------------------------------------------------
+def test_prompt():
+    """
+    'replay -p CMD' should run CMD and show its output when user hits ENTER
+    """
+    pytest.fail('construction')
+
+
+# -----------------------------------------------------------------------------
+def test_change():
+    """
+    'replay CMD' should display the output of CMD when it changes
+    """
+    pytest.fail('construction')
+
+
+# -----------------------------------------------------------------------------
+def test_filepath():
+    """
+    'replay -t FILENAME CMD' should run CMD when timetamp on FILENAME changes
+    """
+    pytest.fail('construction')
+
+
+# -----------------------------------------------------------------------------
 def test_replayNeedsTests():
     """
     Need tests for replay
