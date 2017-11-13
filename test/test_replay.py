5fcc9eef83f248a50af6a2bfc043ef2fe1acac6e
Tom Barron <tusculum@gmail.com>
2017-11-13 11:23:19 -0500

    TEST: Fix up test_filepath

--- test/test_replay.py
+++ test/test_replay.py
@@ -24,13 +24,23 @@ def test_change():
 
 
 # -----------------------------------------------------------------------------
-def test_iterations():
+def test_filepath(tmpdir):
     """
-    'replay -i 7 CMD' should run CMD and show its output 7 times
+    'replay -t FILENAME CMD' should run CMD when timetamp on FILENAME changes
     """
     pytest.debug_func()
+    contents = ["one foo", "two foo", "three foo"]
+    nib = tmpdir.join("nibble")
     count = 3
-    rcmd = "replay -i {} \"date '+%s xxx'\"".format(count)
+    rcmd = "replay -t {} \"cat {}\"".format(nib.strpath, nib.strpath)
+    S = pexpect.spawn(rcmd)
+    for idx in range(count):
+        nib.write(contents[idx] + "\n")
+        S.expect(["foo", pexpect.EOF])
+        assert S.before.strip() in contents[idx]
+    S.close()
+
+
     S = pexpect.spawn(rcmd)
     last = None
     for idx in range(count):
