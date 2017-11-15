f567a3d27658e6ab3af100549ab963f33d2bf6a7
Tom Barron <tusculum@gmail.com>
2017-11-15 07:30:26 -0500

    TEST: Making xclean tests stand alone

--- test/test_xclean.py
+++ test/test_xclean.py
@@ -3,8 +3,83 @@ from bscr import util as U
 import os
 import pexpect
 import pytest
+import re
 import shutil
 from bscr import testhelp as th
+
+
+# -----------------------------------------------------------------------------
+def test_cleanup_dr_np_nr(capsys, tmpdir, fx_cleanup):
+    """
+    Run cleanup with dryrun but no pattern or recursive flag. Afterwards, the
+    target files should still exist. The dryrun message should show up in the
+    output. Target files in the top directory should be named in the output.
+    """
+    pytest.debug_func()
+    bscr.xclean.cleanup(str(tmpdir), dryrun=True)
+    result = "".join(capsys.readouterr())
+    assert fx_cleanup["drmsg"] in result
+    for floc in fx_cleanup["files"]:
+        floc.exists()
+        if all([floc.basename.endswith("~"),
+                tmpdir.strpath == floc.dirname]):
+            assert floc.basename in result
+        else:
+            assert floc.basename not in result
+
+
+# -----------------------------------------------------------------------------
+def test_cleanup_dr_np_r(capsys, tmpdir, fx_cleanup):
+    """
+    xclean --dry-run --recursive
+    """
+    pytest.debug_func()
+    bscr.xclean.cleanup(str(tmpdir), dryrun=True, recursive=True)
+    result = "".join(capsys.readouterr())
+    assert fx_cleanup["drmsg"] in result
+    for floc in fx_cleanup["files"]:
+        floc.exists()
+        if floc.basename.endswith("~"):
+            assert floc.basename in result
+        else:
+            assert floc.basename not in result
+
+
+# -----------------------------------------------------------------------------
+def test_cleanup_dr_p_nr(capsys, tmpdir, fx_cleanup):
+    """
+    xclean --dry-run --pattern 'no.*'
+    """
+    pytest.debug_func()
+    rgx = "no.*"
+    bscr.xclean.cleanup(str(tmpdir), dryrun=True, pattern=rgx)
+    result = "".join(capsys.readouterr())
+    assert fx_cleanup["drmsg"] in result
+    for floc in fx_cleanup["files"]:
+        floc.exists()
+        if all([tmpdir.strpath == floc.dirname,
+                re.findall(rgx, floc.basename)]):
+            assert floc.basename in result
+        else:
+            assert floc.basename not in result
+
+
+# -----------------------------------------------------------------------------
+def test_cleanup_dr_p_r(capsys, tmpdir, fx_cleanup):
+    """
+    xclean --dry-run --pattern 'no.*' --recursive
+    """
+    pytest.debug_func()
+    rgx = "no.*"
+    bscr.xclean.cleanup(str(tmpdir), dryrun=True, pattern=rgx, recursive=True)
+    result = "".join(capsys.readouterr())
+    assert fx_cleanup["drmsg"] in result
+    for floc in fx_cleanup["files"]:
+        floc.exists()
+        if re.findall(rgx, floc.basename):
+            assert floc.basename in result
+        else:
+            assert floc.basename not in result
 
 
 # -----------------------------------------------------------------------------
@@ -13,6 +88,25 @@ def test_standalone():
     Make these tests stand-alone
     """
     pytest.fail("Make {} tests stand-alone".format(__file__))
+
+
+# -----------------------------------------------------------------------------
+@pytest.fixture
+def fx_cleanup(tmpdir):
+    """
+    Set up files for various tests
+    """
+    tmpdir.join('sub').ensure(dir=True)
+    rval = {}
+    rval['drmsg'] = "Without --dryrun, would remove"
+    rval['files'] = [tmpdir.join(x).ensure() for x in ['xxx~',
+                                                       'yyy~',
+                                                       'sub/basement~',
+                                                       'sub/.floor~',
+                                                       'nomatch.txt',
+                                                       'sub/nosuchfile',
+                                                       ]]
+    return rval
 
 
 # -----------------------------------------------------------------------------
@@ -44,83 +138,6 @@ class TestXclean(th.HelpedTestCase):
         Tear down after each test
         """
         shutil.rmtree(self.testdir)
-
-    # -------------------------------------------------------------------------
-    def test_cleanup_dr_np_nr(self):
-        """
-        Run cleanup with dryrun but no pattern or recursive flag. Afterwards,
-        the target files should still exist. The dryrun message should show up
-        in the output. All the target files should be named in the output.
-        """
-        with th.StdoutExcursion() as getval:
-            bscr.xclean.cleanup(self.testdir, dryrun=True)
-            result = getval()
-        self.verify_exists([self.tilde, self.ntilde, self.stilde,
-                            self.nstilde],
-                           [True, True, True, True])
-
-        self.assertIn(self.drmsg, result)
-
-        self.verify_in([self.tilde, self.ntilde, self.stilde, self.nstilde],
-                       result,
-                       [True, False, False, False])
-
-    # -------------------------------------------------------------------------
-    def test_cleanup_dr_np_r(self):
-        """
-        xclean --dry-run --recursive
-        """
-        with th.StdoutExcursion() as getval:
-            bscr.xclean.cleanup(self.testdir, dryrun=True, recursive=True)
-            result = getval()
-        self.verify_exists([self.tilde, self.ntilde, self.stilde,
-                            self.nstilde],
-                           [True, True, True, True])
-
-        self.assertIn(self.drmsg, result)
-
-        self.verify_in([self.tilde, self.ntilde, self.stilde, self.nstilde],
-                       result,
-                       [True, False, True, False])
-
-    # -------------------------------------------------------------------------
-    def test_cleanup_dr_p_nr(self):
-        """
-        xclean --dry-run --pattern "no.*"
-        """
-        with th.StdoutExcursion() as getval:
-            bscr.xclean.cleanup(self.testdir, dryrun=True, pattern="no.*")
-            result = getval()
-
-        self.verify_exists([self.tilde, self.ntilde, self.stilde,
-                            self.nstilde],
-                           [True, True, True, True])
-
-        self.assertIn(self.drmsg, result)
-
-        self.verify_in([self.tilde, self.ntilde, self.stilde, self.nstilde],
-                       result,
-                       [False, True, False, False])
-
-    # -------------------------------------------------------------------------
-    def test_cleanup_dr_p_r(self):
-        """
-        xclean --dry-run --pattern "no.*" --recursive
-        """
-        with th.StdoutExcursion() as getval:
-            bscr.xclean.cleanup(self.testdir, dryrun=True, pattern="no.*",
-                                recursive=True)
-            result = getval()
-
-        self.verify_exists([self.tilde, self.ntilde, self.stilde,
-                            self.nstilde],
-                           [True, True, True, True])
-
-        self.assertIn(self.drmsg, result)
-
-        self.verify_in([self.tilde, self.ntilde, self.stilde, self.nstilde],
-                       result,
-                       [False, True, False, True])
 
     # -------------------------------------------------------------------------
     def test_cleanup_ndr_np_nr(self):
