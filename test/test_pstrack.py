cfd69a42824801ebb495dff42328dc4e55c52346
Tom Barron <tusculum@gmail.com>
2017-11-18 07:35:54 -0500

    TEST: Add test for 'pstrack --help'
        factoring out common info into a fixture, fx_usage

--- test/test_pstrack.py
+++ test/test_pstrack.py
@@ -10,6 +10,26 @@ from bscr import pstrack
 
 
 # -----------------------------------------------------------------------------
+def test_docopt(fx_usage):
+    """
+    Check that pstrack.__doc__ contains what docopt requires
+    """
+    pytest.debug_func()
+    for strval in fx_usage:
+        assert any([strval in line for line in pstrack.__doc__.split("\n")])
+
+
+# -----------------------------------------------------------------------------
+def test_execution():
+    """
+    Does pstrack run successfully?
+    """
+    pytest.debug_func()
+    result = pexpect.run('pstrack')
+    assert 'Traceback' not in result
+
+
+# -----------------------------------------------------------------------------
 def test_get_process_list():
     """
     The dict returned by get_process_list should contain the current process
@@ -24,25 +44,24 @@ def test_get_process_list():
 
 
 # -----------------------------------------------------------------------------
-def test_docopt():
+def test_help(fx_usage):
     """
-    Check that pstrack.__doc__ contains what docopt requires
+    Check what 'pstrack --help' produces
     """
-    pytest.debug_func()
-    reqs = ["Usage:",
+    result = pexpect.run("pstrack --help")
+    for strval in fx_usage:
+        assert any([strval in line for line in result.split("\n")])
+
+
+# -----------------------------------------------------------------------------
+@pytest.fixture
+def fx_usage():
+    """
+    Set up expected help/usage data for tests
+    """
+    rval = ["Usage:",
             "pstrack [-d] [-v]",
             "Options:",
             "-d          debug",
             "-v          verbose"]
-    for strval in reqs:
-        assert any([strval in line for line in pstrack.__doc__.split("\n")])
-
-
-# -----------------------------------------------------------------------------
-def test_execution():
-    """
-    Does pstrack run successfully?
-    """
-    pytest.debug_func()
-    result = pexpect.run('pstrack')
-    assert 'Traceback' not in result
+    return rval
