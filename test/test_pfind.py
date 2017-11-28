10cbe7d7a0369a37c556a794ff1f8ee0a05c2be8
Tom Barron <tusculum@gmail.com>
2017-11-28 18:55:02 -0500

    TEST: Make the timezone explicit...
     - so when the test runs in another timezone, it still passes

--- test/test_pfind.py
+++ test/test_pfind.py
@@ -185,8 +185,8 @@ def test_parse_time():
     an epoch time
     """
     pytest.debug_func()
-    assert pfind.parse_time('2016.0101') == 1451624400.0
-    assert pfind.parse_time('2015.1219.175455') == 1450565695.0
+    assert pfind.parse_time('2016.0101 EST') == 1451624400.0
+    assert pfind.parse_time('2015.1219.175455 EST') == 1450565695.0
 
 
 # -----------------------------------------------------------------------------
